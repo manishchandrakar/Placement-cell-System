b55bc9bac831b6a6a127601062ab65bef0300f75
manishchandrakar <70218033+manishchandrakar@users.noreply.github.com>
2023-05-27 13:25:06 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 async function main(){
-    // await mongoose.connect('mongodb+srv://manishchandrakar2001:PUrRZkY3POiCC5vN@cluster0.w5fmnnm.mongodb.net/placements-cell-db');
-    await mongoose.connect('mongodb+srv://placement:1UbhMXi13RXuCwSn@cluster0.p5gbb.mongodb.net/placementcell?retryWrites=true&w=majority');
+    await mongoose.connect('mongodb+srv://manishchandrakar2001:PUrRZkY3POiCC5vN@cluster0.w5fmnnm.mongodb.net/placements-cell-db');
+//     await mongoose.connect('mongodb+srv://placement:1UbhMXi13RXuCwSn@cluster0.p5gbb.mongodb.net/placementcell?retryWrites=true&w=majority');
 
     console.log("connection Successfull !! ");
 }
